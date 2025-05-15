48170cbf197965f40aabb453a5b9ec4091a3feaa
Gustavo Lepinsk Carvalho <gustavolepinskcarvalho07@gmail.com>
2025-05-14 23:46:55 -0300

    feat: implement Intro component with styling and functionality; fix: conditionally render on Main

---
+++ src/components/Default/Intro.jsx
@@ -0,0 +1,25 @@
+import { useCountryContext } from "../Contexts/CountryContext";
+import "./Intro.css";
+
+export default function Intro({}) {
+  const { setSelectedCountry, countryList } = useCountryContext();
+  return (
+    <div className="intro">
+      <p className="app-title">Geography App</p>
+      <img
+        className="app-image"
+        src="public/earth-globe.png"
+        alt="Earth Globe"
+        onClick={() => setSelectedCountry(countryList[Math.floor(Math.random() * countryList.length)])}
+      />
+      <p className="app-desc">
+        Open the sidebar and search for any country you would like to know more about!
+      </p>
+      <p className="app-desc">
+        Discover detailed information about countries, including population, area, languages, and
+        more!
+      </p>
+      <p className="app-desc">Make sure to also check out the maps and the other features!</p>
+    </div>
+  );
+}
--- src/components/MainContent/MainArea/Main.jsx
+++ src/components/MainContent/MainArea/Main.jsx
@@ -1,15 +1,21 @@
 import "./Main.css";
 import Map from "../../Maps/Map.jsx";
 import FullCountryInfo from "../Info/Full Info/FullCountryInfo.jsx";
+import Intro from "../../Default/Intro.jsx";
 import { useCountryContext } from "../../Contexts/CountryContext.jsx";
 
 export default function Main({ isSidebarOpen }) {
   const { selectedCountry } = useCountryContext();
-  if (!selectedCountry) return null;
   return (
     <main className={`main-content ${!isSidebarOpen ? "closed" : ""}`}>
-      <FullCountryInfo />
-      <Map selectedCountry={selectedCountry} />
+      {selectedCountry ? (
+        <>
+          <FullCountryInfo />
+          <Map />
+        </>
+      ) : (
+        <Intro />
+      )}
     </main>
   );
 }
