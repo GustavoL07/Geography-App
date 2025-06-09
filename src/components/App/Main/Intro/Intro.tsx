8f6c770cc09da59d898c00dbcf557b7cb39e3eee
Gustavo Lepinsk Carvalho <gustavolepinskcarvalho07@gmail.com>
2025-06-08 21:46:08 -0300

    Opens random country when clicking on the intro image

--- src/components/App/Main/Intro/Intro.tsx
+++ src/components/App/Main/Intro/Intro.tsx
@@ -1,10 +1,21 @@
+import { useCountryContext } from "@/components/Contexts/CountryContext";
 import "./Intro.css";
+import { useSettingsContext } from "@/components/Contexts/SettingsContext";
 
 export default function Intro({}) {
+  const { countryList, setSelectedCountry } = useCountryContext();
+  const {setDisplayMode} = useSettingsContext()
+
+  function onImageClick(){
+    const randomCountry = countryList[Math.round(Math.random() * countryList.length)];
+    setSelectedCountry(randomCountry);
+    setDisplayMode("full");
+  }
+
   return (
     <div className="intro">
       <p className="app-title">Geography App</p>
-      <img className="app-image" src="/earth-globe.png" alt="Earth Globe" />
+      <img className="app-image" src="/earth-globe.png" alt="Earth Globe" onClick={() => onImageClick()}/>
       <section>
         <p className="app-desc">
           Open the sidebar and search for any country you would like to know more about!
