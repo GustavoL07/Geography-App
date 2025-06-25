63ec6ac8df252c8053c5c89a4fbcf3bccbcffc13
Gustavo Lepinsk Carvalho <gustavolepinskcarvalho07@gmail.com>
2025-06-25 20:19:18 -0300

    Filter added

--- src/components/App/Sidebar/Sidebar/Top/Top.tsx
+++ src/components/App/Sidebar/Sidebar/Top/Top.tsx
@@ -5,6 +5,7 @@ import Search from "@/components/Custom/Search/Search";
 import Menu from "@/components/Custom/Menu/Menu";
 import Sort from "@/components/Custom/Menu/Sort/Sort";
 import { SortOptions } from "@/utils/Organizing/sorter";
+import Filter from "@/components/Custom/Menu/Filter/Filter";
 
 type Props = {
   isOpen: boolean;
@@ -13,7 +14,7 @@ type Props = {
 export default function Top({ isOpen, toggleIsOpen }: Props) {
   const { sortValue, searchValue, setSearchValue } = useSettingsContext();
 
-  const menuText = SortOptions.find((opt) => opt.key === sortValue)?.text || "Sort...";
+  const sortMenuText = SortOptions.find((opt) => opt.key === sortValue)?.text || "Sort...";
 
   return (
     <div className={`top ${isOpen ? "" : "closed"}`}>
@@ -30,10 +31,16 @@ export default function Top({ isOpen, toggleIsOpen }: Props) {
             />
           </div>
           <div className="top-element">
-            <Menu icon={<i className="fas fa-sort"></i>} text={menuText}>
+            <Menu icon={<i className="fas fa-sort"></i>} text={sortMenuText}>
               {(closeMenu) => <Sort onClose={closeMenu} />}
+            </Menu >
+
+            <Menu icon={<i className="fa-solid fa-filter"></i>} text={"filter"}>
+              {(closeMenu) => <Filter onClose={closeMenu} />}
             </Menu>
+            
           </div>
+          
         </>
       ) : (
         <>
---
+++ src/components/Custom/Menu/Filter/Filter.tsx
@@ -0,0 +1,11 @@
+import "./Filter.css";
+type Props = {
+  onClose: () => void;
+};
+export default function Filter({}: Props) {
+  return (
+    <div className="filter-wrapper">
+      abc
+    </div>
+  );
+}
